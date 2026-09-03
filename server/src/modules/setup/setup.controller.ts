fa3ab02a06d3dccd82c2b62e2a96c4cc73235b23
Franz <franz@altnalabs.com>
2026-09-03 14:45:52 +0800

    Make last name optional on the owner bootstrap endpoint
    Some accounts only have a single name to give.

--- server/src/modules/setup/setup.controller.ts
+++ server/src/modules/setup/setup.controller.ts
@@ -8,7 +8,7 @@ const bootstrapSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
   firstName: z.string().min(1),
-  lastName: z.string().min(1),
+  lastName: z.string().optional().default(""),
   companyName: z.string().min(1),
 });
 
